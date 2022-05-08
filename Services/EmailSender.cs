1ed46281843865313f964aff544cb4d213d8c1d7
mirotorch <93072176+mirotorch@users.noreply.github.com>
2022-05-08 11:43:59 +0200

    Update EmailSender.cs

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -33,7 +33,7 @@ public class EmailSender : IEmailSender
         var client = new SendGridClient(apiKey);
         var msg = new SendGridMessage()
         {
-            From = new EmailAddress("ruslan.kovalenko@nure.ua", "Password Recovery"),
+            From = new EmailAddress("countryhouse.service.demo@gmail.com", "Password Recovery"),
             Subject = subject,
             PlainTextContent = message,
             HtmlContent = message
