82fc615dfb44fbee712e23ebf0878f7403e82cc6
mirotorch <93072176+mirotorch@users.noreply.github.com>
2022-05-08 11:43:34 +0200

    Update AuthMessageSenderOptions.cs

--- Services/AuthMessageSenderOptions.cs
+++ Services/AuthMessageSenderOptions.cs
@@ -3,6 +3,6 @@
     public class AuthMessageSenderOptions
     {
         //add your key here
-        public string? SendGridKey { get; set; } = "";
+        public string? SendGridKey { get; set; } = "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";
     }
 }
